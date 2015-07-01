de3733df01cde55cbf4ffa7e7df60095216de53d
Victor Areba <vicgoburg@gmail.com>
2015-07-01 07:03:26 +0300

    Paginate Snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -4,7 +4,7 @@ class SnippetsController < ApplicationController
   # GET /snippets
   # GET /snippets.json
   def index
-    @snippets = Snippet.all
+    @snippets = Snippet.paginate(:page => params[:page], :per_page => 20)
   end
 
   # GET /snippets/1
