1aae0d80ad2a92bbef915b2f62c9828ba085d66d
Victor Areba <vicgoburg@gmail.com>
2015-07-01 06:58:22 +0300

    Limit snippets to 15 words on index

--- db/migrate/20150701032231_create_snippets.rb
+++ db/migrate/20150701032231_create_snippets.rb
@@ -2,7 +2,7 @@ class CreateSnippets < ActiveRecord::Migration
   def change
     create_table :snippets do |t|
       t.text :content
-      t.boolean :private
+      t.boolean :private, default: false
 
       t.timestamps null: false
     end
