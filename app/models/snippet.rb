a56081d3d13f6c21dddaa4be669cc7df771419d1
Victor Areba <vicgoburg@gmail.com>
2015-07-01 19:40:30 +0300

    Token gen logic change

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,7 +1,6 @@
 class Snippet < ActiveRecord::Base
   validates_length_of :content, maximum: 8192
   validates_presence_of :content
-  validates_uniqueness_of :token
   scope :public_snippets, ->  {where(private: false)}
 
   after_create :generate_token, if: Proc.new { self.private? }
@@ -9,11 +8,12 @@ class Snippet < ActiveRecord::Base
   def generate_token
     begin
       self.token = SecureRandom.urlsafe_base64(64, false)
-      save!
-    end
+    end while Snippet.exists?(token: token)
+    save!
   end
 
   def self.search(search)
     where("content like ?", "%#{search}%")
   end
 end
+
--- test/models/snippet_test.rb
+++ test/models/snippet_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class SnippetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have the necessary required validators" do
+    snippet = Snippet.new
+    assert_not snippet.valid?
+    assert_equal [:content], snippet.errors.keys
+  end
 end
