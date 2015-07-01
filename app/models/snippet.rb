77b24d3c80307bc742587fc3d7754afd585de306
Victor Areba <vicgoburg@gmail.com>
2015-07-01 09:00:51 +0300

    Token gem changes

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,13 +2,13 @@ class Snippet < ActiveRecord::Base
   validates_length_of :content, maximum: 8192
   scope :public_snippets, ->  {where(private: false)}
 
-  before_save :generate_token, if: Proc.new { self.private? }
+  after_create :generate_token, if: Proc.new { self.private? }
 
   def generate_token
     begin
       self.token = SecureRandom.urlsafe_base64(64, false)
       save!
-    end while self.class.find_by(token: token)
+    end
   end
 
   def self.search(search)
