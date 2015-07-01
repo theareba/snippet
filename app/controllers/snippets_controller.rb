e3e2d1cc6ffc2e67369d1ac1913cf4ad0ae61b4e
Victor Areba <vicgoburg@gmail.com>
2015-07-01 09:48:36 +0300

    Include token in find logic

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -69,7 +69,12 @@ class SnippetsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_snippet
-      @snippet = Snippet.find(params[:id])
+      param = Integer(params[:id]) rescue nil
+      if param.nil?
+        @snippet = Snippet.find_by_token(params[:id])
+      else
+        @snippet = Snippet.find(params[:id])
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
